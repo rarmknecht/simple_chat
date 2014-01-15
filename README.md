73d57e853cb41022fc28f1e74ce528b1cfa87213
rarmknecht <rarmknecht@gmail.com>
2014-01-14 22:31:23 -0600

    Switched to twisted network library

--- README.md
+++ README.md
@@ -7,11 +7,17 @@ Playground Application to Practice Various Concepts
  - Message Queuing
  - Git Branches
 
-~~~~~~~~~~~
-Unit Testing
-~~~~~~~~~~
+###Network Traffic
+While researching how to support non-blocking IO, I discovered that there is a general purpose networking library called `Twisted <http://twistedmatrix.com/trac/`.  This library is event-driven and covers many of the corner cases that would take a significant amount of time to discover, understand, and address.
+
+It can be installed with: `pip install twisted`
+
+###Unit Testing
 Unit testing is now supported.  Main check is for conformance to the PEP8 Style Guide.
 
 Run the test be executing: `python -m unittest discover`
 
-The unittest and pep8 modules are required.
+The unittest and pep8 modules are required. Both can be installed through pip:
+    
+    $ pip install unittest
+    $ pip install pep8
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-import sys
-import socket
+from twisted.internet import protocol, reactor
 
 # TODO - Multi-threaded support
 # TODO - Port as command line option
@@ -9,42 +8,14 @@ import socket
 # TODO - Message Polling from Clients
 
 
-def run_server(_host='', _port=5151):
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
 
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("* Socket Created")
-    try:
-        serversocket.bind((_host, _port))
-    except socket.error as msg:
-        print("E Bind Failed.\nE\t%s" % str(msg))
-        sys.exit(1)
-    print("* Socket Bind Complete")
-    serversocket.listen(5)
-    print("* Socket Now Listening on Port %s" % str(_port))
-
-    while True:
-        try:
-            (clientsocket, address) = serversocket.accept()
-            print("* Connected with %s:%s" % (str(address[0]),
-                                              str(address[1])))
-
-            data = clientsocket.recv(1024)
-            if not data:
-                break
-
-            print("# Received: %s" % str(data))
-
-            response = "Thank you!\n"
-            clientsocket.sendall(response.encode("utf-8"))
-            print("* Gave Thanks")
-
-            clientsocket.close()
-            print("* Client Closed")
-        except(KeyboardInterrupt, SystemExit):
-            print("\r* Caught Ctrl-C")
-            serversocket.close()
-            print("* All Sockets Closed")
-            sys.exit(0)
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self,addr):
+        return Echo()
 
 if __name__ == "__main__":
-    run_server()
+    reactor.listenTCP(5151, EchoFactory())
+    reactor.run()
