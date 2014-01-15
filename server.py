94aee9e5822b2b8422b5fd69529f1abd3352eb6c
rarmknecht <rarmknecht@gmail.com>
2014-01-14 22:33:30 -0600

    PEP8 conformance

--- server.py
+++ server.py
@@ -12,8 +12,9 @@ class Echo(protocol.Protocol):
     def dataReceived(self, data):
         self.transport.write(data)
 
+
 class EchoFactory(protocol.Factory):
-    def buildProtocol(self,addr):
+    def buildProtocol(self, addr):
         return Echo()
 
 if __name__ == "__main__":
